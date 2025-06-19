5aafb60e3662757e895156c30b2820b6cd17fb65
zaicket <vargas.pedro@pucp.edu.pe>
2025-06-19 18:24:23 -0500

    Finalizar versiones para FuaEstado

---
+++ api/src/main/java/org/openmrs/module/fua/api/dao/FuaEstadoVersionDao.java
@@ -0,0 +1,23 @@
+package org.openmrs.module.fua.api.dao;
+
+import org.openmrs.module.fua.FuaEstadoVersion;
+import org.openmrs.api.db.hibernate.DbSession;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("fua.FuaEstadoVersionDao")
+public class FuaEstadoVersionDao {
+	
+	@Autowired
+	private DbSessionFactory sessionFactory;
+	
+	private DbSession getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+	
+	public FuaEstadoVersion saveFuaEstadoVersion(FuaEstadoVersion version) {
+		getSession().saveOrUpdate(version);
+		return version;
+	}
+}
